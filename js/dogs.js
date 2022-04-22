1b8011c81ca3b3655d128a1e2ae13052e0308fd8
Christopher Rempe <43194904+MrRicearonie@users.noreply.github.com>
2022-04-21 20:55:19 -0700

    Code now follows AirBNB JS rules

--- js/dogs.js
+++ js/dogs.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-alert */
 let total = 0;
 
-// enlint-disable-next-line no-unused-vars
-function alertDogInfo (name, breed, price) {
-    alert(`Hi! I\'m ${name}\nI am a ${breed}\nAnd I cost $${price} to adopt`);
+// eslint-disable-next-line no-unused-vars
+function alertDogInfo(name, breed, price) {
+  alert(`Hi! I'm ${name}\nI am a ${breed}\nAnd I cost $${price} to adopt`);
 }
 
-// enlint-disable-next-line no-unused-vars
-function addDog (price) {
-    total += Number(price);
-    alert(`Your total is now: $${total.toFixed(2)}`);
+// eslint-disable-next-line no-unused-vars
+function addDog(price) {
+  total += Number(price);
+  alert(`Your total is now: $${total.toFixed(2)}`);
 }
