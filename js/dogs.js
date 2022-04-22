36672d37b009cea9baba1bd22c7b7b7b32616c40
Christopher Rempe <43194904+MrRicearonie@users.noreply.github.com>
2022-04-21 20:09:41 -0700

    Slightly changed the wording

--- js/dogs.js
+++ js/dogs.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-alert */
 // enlint-disable-next-line no-unused-vars
 function alertDogInfo (name, breed, price) {
-    alert(`Hi! I\'m: ${name}\nI am a: ${breed}\nAnd I cost: $${price}`);
+    alert(`Hi! I\'m ${name}\nI am a ${breed}\nAnd I cost $${price} to adopt`);
 }
